52d682907e19254da9285125d069fd000a5dc222
nour khoury <khourynour79@gmail.com>
2022-09-28 17:13:27 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
   
+
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit',(event) =>{
+        event.preventDefault()
+      // event.target.children[1].value//Accesing via index
+      const input = document.querySelector('input#searchByID');
+
+      console.log(input.value);
+
+      fetch('http://localhost:3000/movies')
+      .then(response => response.json()
+      .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+    summary.innerText = data.summary;
+      }));
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
